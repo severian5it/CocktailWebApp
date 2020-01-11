c2cf6b65b188827455eb28e3dfa413f39e5c867e
Pierluca <pierluca@amazon.com>
2020-01-11 19:45:28 +0000

    adding search box

--- src/js/main.js
+++ src/js/main.js
@@ -11,15 +11,6 @@ function getData(url, cb) {
     xhr.send();
 }
 
-function getTableHeaders(obj) {
-    var tableHeaders = [];
-
-    Object.keys(obj).forEach(function(key) {
-        tableHeaders.push(`<td>${key}</td>`);
-    });
-
-    return `<tr>${tableHeaders}</tr>`;
-}
 
 function writeToDocument(url) {
     var tableRows = [];
@@ -27,13 +18,9 @@ function writeToDocument(url) {
 
     getData(url, function(data) {
         data = data.drinks;
-        var tableHeaders = getTableHeaders(data[0]);
 
         data.forEach(function(item) {
             var dataRow = [];
-            console.log('data', data)
-            console.log('item', item)
-            console.log('Object', Object)
 
             dataRow.push(`<td>${item.strDrink}</td>`);
             dataRow.push(`<td><img src=${item.strDrinkThumb}></td>`);
@@ -41,6 +28,17 @@ function writeToDocument(url) {
             tableRows.push(`<tr>${dataRow}</tr>`);
         });
 
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+        el.innerHTML = `<table>${tableRows}</table>`.replace(/,/g, "");;
+        console.log(`<table>${tableRows}</table>`)
+
     });
 }
+
+
+function searchSubmit() {
+  let url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=';
+  var inputVal = document.getElementById("toSearch").value;
+  console.log("input", inputVal);
+  writeToDocument(url + inputVal)
+}
+

74ab6766c914531f254584943d9a64296e494438
Pierluca Del Buono <pierluca@amazon.com>
2020-03-01 18:18:00 +0100

    Check contact Form Validity via jQuery

--- src/js/mail.js
+++ src/js/mail.js
@@ -1,7 +1,15 @@
 
 function sendMail() {
-    let name = document.getElementById("form34").value;
-    let mail = document.getElementById("form29").value;
-    let message = document.getElementById("form8").value;
-    emailjs.send("gmail", "cocktail", {"from_name":`${name}`,"from_email":`${mail}`,"message":`${message}`})
+    let name = document.getElementById("form34");
+    let mail = document.getElementById("form29");
+    let message = document.getElementById("form8");
+    if (name.checkValidity() && mail.checkValidity() && message.checkValidity()) {
+        emailjs.send("gmail", "cocktail", {
+            "from_name": `${name.value}`,
+            "from_email": `${mail.value}`,
+            "message": `${message.value}`
+        })
+        $('#modalContactForm').modal('toggle');
+    }
+
 }

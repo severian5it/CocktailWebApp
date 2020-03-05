68e6009b80aba500de70e964797a78c32d428b78
Pierluca Del Buono <pierluca@amazon.com>
2020-03-05 22:38:12 +0100

    col6 and changing dimensions in landing page

--- assets/js/apiCall.js
+++ assets/js/apiCall.js
@@ -20,8 +20,8 @@ function writeToDocument(url) {
         data = response.drinks;
         data.forEach(function (item) {
             let dataRow = [];
-            dataRow.push('<div class="col-sm-6 col-md-4 col-lg-2 ">')
-            dataRow.push('<div class="card shadow-lg p-3 mb-5 bg-white rounded">')
+            dataRow.push('<div class="col-sm-6 col-md-4 col-lg-3 ">')
+            dataRow.push('<div class="card shadow-lg p-3 mb-5 bg-white rounded mt-4">')
             dataRow.push('<div class="image">')
             dataRow.push(`<img src=${item.strDrinkThumb} onclick="openGalleryModal(${item.idDrink})">`)
             dataRow.push('</div>')
